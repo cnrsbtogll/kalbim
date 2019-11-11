173042f0635c6565d476cf2fe31904547364ecf0
Caner_Sabitogullari <caner.sabitogullari2@ogr.sakarya.edu.tr>
2019-11-12 01:06:42 +0300

    Login page created

---
+++ RN_Kalbim/src/screens/SignUp/validations.js
@@ -0,0 +1,17 @@
+import * as Yup from "yup";
+
+const validations = Yup.object().shape({
+	email: Yup
+		.string()
+		.email()
+		.required(),
+	password: Yup
+		.string()		
+		.required(),
+	passwordConfirm: Yup
+		.string()
+		.oneOf([Yup.ref('password')], 'Passwords not matched.')
+		.required()
+});
+
+module.exports = validations;
