3a9f6688176669e323af2b2dfcc59e8c422e5e22
cnrsbtogll <ankara_can_19@hotmail.com>
2020-01-24 15:06:44 +0300

    redgoogleloginbutton add

--- RN_Kalbim/src/styles/colors/index.js
+++ RN_Kalbim/src/styles/colors/index.js
@@ -3,6 +3,7 @@ export default {
     white: '#ffffff',
     bluebackground: '#1F2640',
     bluefacebookloginbutton:'#4162A6',
+    redgoogleloginbutton:'#DD4F43',
     blueloginbutton:'#13C9F2',
     tabcontainercolor:'#151515',
     boxcolor:'#2B3043',
